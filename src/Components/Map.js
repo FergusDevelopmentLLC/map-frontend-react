16267a56681399c29287afe4c84e50881c909e91
Will Carter <will.carter@fergusllc.com>
2021-03-06 23:56:52 -0700

    add calls to backend

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -9,6 +9,8 @@ const Map = ({
 
   const mapContainer = useRef(null)
   const [statefulMap, setMap] = useState(null)
+  const [states, setStates] = useState([])
+  const [geoJSON, setgeoJSON] = useState(null)
 
   useEffect(() => {
 
@@ -36,11 +38,70 @@ const Map = ({
     if (!statefulMap) {
       initializeMap()
     }
+    else {
+      if(geoJSON) {
+        
+        statefulMap.addSource('aoi', {
+          type: 'geojson',
+          data: geoJSON
+        })
 
-  }, [statefulMap, center, zoom])
+        statefulMap.addLayer({
+          id: 'aoi-layer',
+          source: 'aoi',
+          type: 'fill'
+        })
+      }
+    }
+
+  }, [statefulMap, center, zoom, geoJSON])
+
+  useEffect(() => {
+
+    fetch("https://8450cseuue.execute-api.us-east-1.amazonaws.com/production/states")
+      .then((res) => {
+        res.json()
+          .then(states => setStates(states))
+          .catch(error => console.log('error', error))
+      })
+      .catch(error => console.log('error', error))
+
+    }, [])
+  
+  const stateChange = (event) => {
+    console.log('event.target.value', event.target.value)
+
+    fetch("https://8450cseuue.execute-api.us-east-1.amazonaws.com/production/getGeoJsonForCsv",{
+      method: 'POST',
+      body: JSON.stringify({
+        "csvUrl": "https://gist.githubusercontent.com/FergusDevelopmentLLC/b95090d5c494ced48a1610c3e954a382/raw/1ef9f8c9819554ab103aebd35fa93f0e63593b34/animal_hospitals_usa.csv",
+        "stusps": event.target.value,
+        "data_description": "test data"
+      })
+    })
+      .then((res) => {
+        res.json()
+          .then(geojson => {
+            setgeoJSON(geojson)
+          })
+          .catch(error => console.log('error', error))
+      })
+      .catch(error => console.log('error', error))
+  }
 
   return (
-    <div ref={mapContainer} className="map-container"></div>
+    <div ref={mapContainer} className="map-container">
+      <div className="dropdown-container">
+        <select onChange={(event) => { stateChange(event) }} >
+          <option>-Select state-</option>
+          {
+            states.map((state, i)=> {
+              return <option key={i} value={ state.stusps }>{ state.name }</option>
+            })
+          }
+        </select>
+      </div>
+    </div>
   )
 }
 
